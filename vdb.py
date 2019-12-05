5b86786d3d8f2976a3dcf1339adbf912b2954173
ngrande <devlup@outlook.com>
2019-12-05 16:49:37 +0100

    Add comments

--- vdb.py
+++ vdb.py
@@ -4,8 +4,11 @@
 print("Loading vdb core")
 
 try:
+    # in case the user has installed via setup.py 
+    # nothing more than an import vdb is necsesary
     import vdb
 except ModuleNotFoundError:
+    # otherwise we add the vdb dir to the python path
     import sys
     from os import path,walk
     directory, file = path.split(__file__)
