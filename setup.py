88d95ea419eeb203f727cb9df58a2e371fc12920
ngrande <devlup@outlook.com>
2019-12-13 14:08:50 +0100

    Adjust setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version="1.0",
 	license=open("LICENSE", "r").read(),
 	long_description=open("README.md", "r").read(),
-	author="Dennis Lubert",
+	author="",
 	author_email="",
 	url="https://github.com/plasmahh/vdb",
 	install_requires=[entry for entry in open("requirements.txt", "r").read().split("\n") if len(entry) > 0],
