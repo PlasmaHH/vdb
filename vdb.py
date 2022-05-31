63f9c87225b3b542f4a1efc6e3678751cae74f20
PlasmaHH <plasmahh@projectiwear.org>
2022-05-31 23:19:53 +0200

    Prevent confusing exception chaining

--- vdb.py
+++ vdb.py
@@ -3,11 +3,17 @@
 
 print("Loading vdb core")
 
+not_found = False
+
 try:
     # in case the user has installed via setup.py 
     # nothing more than an import vdb is necsesary
     import vdb
 except ModuleNotFoundError:
+    not_found = True
+
+# Don't do it in the except clause since it may throw an exception which then gets chained which is confusing sometimes
+if( not_found ):
     # otherwise we add the vdb dir to the python path
     import sys
     from os import path,walk
