75a0ee49cfdd1de7f00c55fda9807afea83f8e00
PlasmaHH <plasmahh@projectiwear.org>
2023-03-16 17:36:27 +0100

    Added function to get some unsigned integer with a certain bitsize

--- vdb/arch.py
+++ vdb/arch.py
@@ -8,12 +8,29 @@ import gdb
 pointer_size = 0
 gdb_uintptr_t = None
 need_update = True
+need_uint_update = True
 
 @vdb.event.new_objfile()
 @vdb.event.new_thread()
 def reset_info():
     global need_update
+    global need_uint_update
     need_update = True
+    need_uint_update = True
+
+uint_cache = {}
+def uint( sz ):
+    global uint_cache
+    global need_uint_update
+    if( need_uint_update or len(uint_cache) == 0 ):
+        for t in [ "uint8_t", "uint16_t", "uint32_t", "uint64_t", "uint128_t", "unsigned char", "unsigned short", "unsigned", "unsigned long", "unsigned long long" ]:
+            try:
+                ty=gdb.lookup_type(t)
+                uint_cache[int(ty.sizeof)*8] = ty
+            except gdb.error as e:
+                pass
+        need_uint_update = False
+    return uint_cache.get(sz,None)
 
 @vdb.event.new_objfile()
 def gather_info( ):
