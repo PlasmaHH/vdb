714ed65aaf90e1f4ca5137dbc78a7f4ec0eefbfc
PlasmaHH <plasmahh@gmx.net>
2023-03-23 16:36:45 +0100

    A few things figured out for real world examples

--- vdb/svd.py
+++ vdb/svd.py
@@ -12,18 +12,23 @@ import gdb.types
 
 import os
 import traceback
+import re
 
 
 auto_scan = vdb.config.parameter("vdb-svd-auto-scan",True,docstring="scan configured directories on start")
-scan_dirs = vdb.config.parameter("vdb-svd-directories",".,~/Downloads/",gdb_type=vdb.config.PARAM_ARRAY )
+scan_dirs = vdb.config.parameter("vdb-svd-directories","~/Downloads/",gdb_type=vdb.config.PARAM_ARRAY )
 scan_recur= vdb.config.parameter("vdb-svd-scan-recursive",True,docstring="Whether to scan directories recursively")
-scan_background = vdb.config.parameter("vdb-svd-scan-background",True,docstring="Do the scan in the background")
+scan_background = vdb.config.parameter("vdb-svd-scan-background",False,docstring="Do the scan in the background")
+scan_filter = vdb.config.parameter("vdb-svd-scan-filter","",docstring="Regexp to filter file names before loading")
 
 
+verbose = False
 try:
-    from defusedxml.ElementTree import parse
+    import defusedxml.ElementTree as ET
+#    from defusedxml.ElementTree import parse
 except:
-    from xml.etree.ElementTree import parse
+    import xml.etree.ElementTree as ET
+#    from xml.etree.ElementTree import parse
 
 def svd_load(idname):
     d = devices.get(idname,None)
@@ -69,6 +74,7 @@ class svd_device:
 
         def __init__( self ):
             self.name = None
+            self.display_name = None
             self.mmap_address = None
             self.bit_size = None
             self.description = {}
@@ -77,12 +83,24 @@ class svd_device:
             self.access = None
             self.type = None
 
+        def _dump( self ):
+            addr=self.mmap_address
+            if( addr is None ):
+                addr = 0
+            print(f"{self.display_name} {self.name} @{addr:#0x}[{self.bit_size}]")
+            for pos,t5 in self.description.items():
+                sz,name,desc,_,amap = t5
+                print(f"    {name}[{sz}]\t{desc} {amap=}")
+
         def _parse_field( self, node ):
             pos  = None
             sz   = None
             name = None
             desc = None
             access = None
+
+            if( node.tag != "field"):
+                raise RuntimeError(f"_parse_field() accepts only 'field' but got '{node.tag}'")
 
             for n in node:
                 match(n.tag):
@@ -94,7 +112,41 @@ class svd_device:
                         pos = vdb.util.rxint(n.text)
                     case "bitWidth":
                         sz = vdb.util.rxint(n.text)
+                    case "access":
+                        access=n.text
+                    case "enumeratedValues":
+                        # TODO the register description already can handle something like this, parse it and check for
+                        # compatibility and when to output what and when to output what
+                        pass
+                    case "bitRange":
+                        n.text = n.text.strip()
+                        if( n.text[0] != "[" or n.text[-1] != "]" ):
+                            print(f"Unknown bitRange format '{n.text}'")
+                            continue
+                        st,en = n.text[1:-1].split(":")
+                        st = int(st)
+                        en = int(en)
+                        if( st > en ):
+                            t=st
+                            st=en
+                            en=t
+                        pos = st
+                        sz = (en-st)+1
+                    # TODO here we have to take the name and generate multiple register fields out of it ( same for
+                    # registers ). See e.g. https://siliconlabs.github.io/Gecko_SDK_Doc/CMSIS/SVD/html/group__dim_element_group__gr.html
+                    case "dimIncrement":
+                        pass
+                    case "dimIndex":
+                        pass
+                    case "dim":
+                        pass
+                    case "writeConstraint": # TODO need to figure out what this is
+                        pass
+                    case "readAction": # afaik we can ignore this for our needs
+                        pass
                     case _:
+                        print("n.tag = '%s'" % (n.tag,) )
+                        print("n.text = '%s'" % (n.text,) )
                         pass
             self.description[pos] = ( sz, name, desc, None, access_map(access) )
 
@@ -106,11 +158,21 @@ class svd_device:
 #            print("self.name = '%s'" % (self.name,) )
 #            print("self.description = '%s'" % (self.description,) )
 
+        def get_short_name( self ):
+            if( self.display_name is None ):
+                return self.name
+            if( len(self.display_name) < len(self.name) ):
+                return self.display_name
+            return self.name
+
+        
         def parse_xml( self, node, base_address ):
             for n in node:
                 match(n.tag):
                     case "name":
                         self.name = n.text
+                    case "displayName":
+                        self.display_name = n.text
                     case "addressOffset":
                         self.mmap_address = base_address + vdb.util.rxint(n.text)
                     case "resetValue":
@@ -137,37 +199,85 @@ class svd_device:
         self.name = None
         self.registers = []
         self.register_names = set()
+        self.cpu = svd_device.cpu_description()
+        self.group_bit_size = None
 
     def load( self, unload = True ):
         print(f"Loading {len(self.registers)} register descriptions")
         if( unload ):
             vdb.register.mmapped_descriptions = {}
             vdb.register.mmapped_positions = {}
+        skip=0
         for r in self.registers:
-            # TODO The 32 must be limited to the real relevant value
-            vdb.register.mmapped_descriptions[r.name] = (r.bit_size, r.description, None )
-            vdb.register.mmapped_positions[r.name] = ( r.mmap_address, r.bit_size, r.type )
+            name=r.get_short_name()
+            if( verbose ):
+                r._dump()
+            if( r.mmap_address is None ):
+                skip += 1
+            else:
+                bitsize = r.bit_size
+                rtype=r.type
+                if( bitsize is None ):
+                    print(f"{r.name} has no bit_size")
+                # TODO configureable? intresting at all?
+#                if( bitsize is None ):
+#                    bitsize = 32
+#                    rtype=vdb.arch.uint(bitsize)
+
+                vdb.register.mmapped_descriptions[name] = (bitsize, r.description, None )
+                vdb.register.mmapped_positions[name] = ( r.mmap_address, bitsize, rtype )
+        if( skip > 0 ):
+            print(f"Skipped {skip} registers due to unkonwn mapping position.")
 
     def _parse_name( self, node ):
         self.name = node.text
-        self.cpu = svd_device.cpu_description()
 
-    def _parse_cpu( self, node ):
+    def _parse_group( self, node ):
         for tag in node:
             match(tag.tag):
+                case "registers":
+                    self._parse_registers(tag,0)
+                case "peripherals":
+                    self._parse_peripherals(tag)
+                case "size":
+                    self.group_bit_size = int(tag.text)
+#                case _:
+#                    print("tag.tag = '%s'" % (tag.tag,) )
+        self.group_bit_size = None
+
+    def _parse_groups( self, node ):
+        for tag in node:
+            match(tag.tag):
+                case "group":
+                    self._parse_group(tag)
+
+    def _parse_cpu( self, node ):
+        dp_name=None
+        for tag in node:
+            match(tag.tag):
+                case "groups":
+                    self._parse_groups(tag)
                 case "name":
                     self.cpu.name = tag.text
+                case "displayName":
+                    dp_name = tag.text
                 case "revision":
                     self.cpu.revision = tag.text
                 case _:
 #                    print("tag.tag = '%s'" % (tag.tag,) )
                     pass
+        if( dp_name is not None and self.cpu.name is not None ):
+            self.cpu_description.cpu_map[self.cpu.name] = dp_name
+#        if( dp_name is not None and self.cpu.name is None ):
+#            print("Display name cpu only {dp_name}")
 
     def _parse_register( self, node, base_address ):
         reg = svd_device.register()
         reg.parse_xml(node,base_address)
         if( reg.name in self.register_names ):
             print(f"Duplicate register name {reg.name}")
+        if( reg.bit_size is None and self.group_bit_size is not None ):
+            reg.bit_size = self.group_bit_size
         self.registers.append(reg)
 
     def _parse_registers( self, node, base_address ):
@@ -175,6 +285,8 @@ class svd_device:
             match(r.tag):
                 case "register":
                     self._parse_register(r,base_address)
+                case "cluster":# TODO Wait, whats that? We need to support them for the stm32-svd tinygo files
+                    pass
                 case _:
                     print("r.tag = '%s'" % (r.tag,) )
 
@@ -215,22 +327,44 @@ class svd_device:
 def parse_device(xml):
     ndev = svd_device()
     ndev.parse_from_xml(xml)
+    if( ndev.name is None ):
+        ndev.name = ndev.cpu.name
     return ndev
 
 def svd_load_file(fname,at):
     if( at is None ):
-        print(f"Loading {fname}")
+        print(f"Loading {fname}… ",end="")
 
-    xml = parse(fname)
+    with open(fname,"r") as f:
+        data = f.read()
+        data = data.replace(" & "," &amp; ");
+    if( len(data) < 16 ):
+        print("File too small, does it contain anything?")
+        return None
+#    xml = ET.parse(fname)
+    xml = ET.ElementTree(ET.fromstring(data))
+
     root = xml.getroot()
     ndev=parse_device(root)
-    global devices
-    devices[ndev.name] = ndev
+    if( ndev.name is None ):
+        print(" contains no named CPU, discarding")
+    else:
+        print(f"=> {ndev.name}")
+        global devices
+        devices[ndev.name] = ndev
 
-def svd_list():
+def svd_list( flt = None):
     otbl = []
     otbl.append( ["Name","CPU","Registers"] )
+    if( flt is not None ):
+        flt_re = re.compile(flt)
     for d in devices.values():
+        if( flt is not None ):
+            m = flt_re.search(d.name)
+            if( m is None ):
+                m = flt_re.search(d.cpu.get_name())
+                if( m is None ):
+                    continue
         line = []
         line.append(d.name)
         line.append(d.cpu.get_name() )
@@ -247,7 +381,12 @@ def do_svd_scan_one(dirname,at):
             if( f.endswith(".svd") ):
                 fullpath = root + "/" + f
                 pathlist.append(fullpath)
+    filter_re = None
+    if( scan_filter.value is not None and len(scan_filter.value) > 0 ):
+        filter_re = re.compile(scan_filter.value)
     for i,p in enumerate(pathlist):
+        if( filter_re is not None and filter_re.search(p) is None ):
+            continue
         if( at is not None ):
             at.set_progress(f"[svd {i}/{len(pathlist)}]")
         try:
@@ -255,7 +394,6 @@ def do_svd_scan_one(dirname,at):
         except:
             traceback.print_exc()
             print(f"Failed to load {p}")
-
 
 def do_svd_scan(at):
 #    print("at = '%s'" % (at,) )
@@ -278,11 +416,18 @@ def svd_scan():
     else:
         do_svd_scan(None)
 
+# XXX mv to some "at first promp" logic
 def start():
     if( not auto_scan.value ):
         print("svd not auto scanning due to vdb-svd-auto-scan False")
         return
     svd_scan()
+
+def get( ar, ix, alt ):
+    try:
+        return ar[ix]
+    except:
+        return alt
 
 class cmd_svd(vdb.command.command):
     """Manage SVD file loading and the interaction with the register display
@@ -315,6 +460,12 @@ svd scan      - Scan configured list of directories and (re)reads the found svd 
             raise gdb.GdbError('svd takes arguments.')
 
         try:
+            global verbose
+            verbose = False
+            if( argv[0] == "/v" ):
+                verbose = True
+                argv=argv[1:]
+
             subcmd = argv[0]
             match(subcmd):
                 case "load":
@@ -322,7 +473,7 @@ svd scan      - Scan configured list of directories and (re)reads the found svd 
                         raise RuntimeError("load needs CPU parameter")
                     svd_load(argv[1])
                 case "list":
-                    svd_list()
+                    svd_list(get(argv,1,None))
                 case "scan":
                     svd_scan()
                 case _:
