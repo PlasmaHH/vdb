472565e7c802e5523d14deb90d87d9daf6a7c462
Dennis Lubert <dennis.lubert@samsongroup.com>
2024-05-21 14:03:25 +0200

    Fixed usage in dashboard

--- .vdb/dashboard/tmux.py
+++ .vdb/dashboard/tmux.py
@@ -3,9 +3,11 @@
 
 import vdb.config
 
+# Start with tmux new-session\; select-pane -T "source"\; split-window -v\; select-pane -T "disassembler" \; split-window -v\; select-pane -T "hexdump" \; split-window -h \; select-pane -T "registers"
 vdb.config.execute("""
 dash tmux disassembler dis/5,15
 dash tmux registers reg/I
 dash tmux ^hexdump$ hd $sp
+dash tmux ^source$ List
 """)
 # vim: tabstop=4 shiftwidth=4 expandtab ft=python
