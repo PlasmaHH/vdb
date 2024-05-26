547dbb2d2cd6851300a663b63538bed70ca65021
PlasmaHH <plasmahh@projectiwear.org>
2024-05-26 23:40:50 +0200

    remvoed accidental paste

--- vdb/unwind.py
+++ vdb/unwind.py
@@ -497,8 +497,7 @@ class cmd_unwind (vdb.command.command):
                 if( argv[0] == "enable" ):
                     enable()
                 elif( argv[0] == "disable" ):
-                    disable()f( before is None and after is None ):
-                        after = before = default_context.value
+                    disable()
                 elif( argv[0] == "hide" ):
                     hide(argv[1:])
                 elif( argv[0] == "fix" ):
